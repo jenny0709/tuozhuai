0ba9dd531c394defe9a51943f7e591568411bb11
mhz520 <1874798077@qq.com>
2016-04-29 09:23:50 +0800

    add

--- indexV2.js
+++ indexV2.js
@@ -30,7 +30,7 @@ window.onload=function(){
             lenX=mx-dx;
             fangxiang=lenX>0?'right':'left';
             bigBox.style.transition="none";
-            bigBox.style.webkitTransform="translateX("+(moveX+lenX)+"px)";
+            bigBox.style.webkitTransform="translate3d("+(moveX+lenX)+"px)";
         }
         document.addEventListener(mouseup,up);
         function up(e){
@@ -51,9 +51,9 @@ window.onload=function(){
                     }
                     moveX=-i*iw;
                     bigBox.style.transition="transform 1s";
-                    bigBox.style.webkitTransform="translateX("+moveX+"px)";
+                    bigBox.style.webkitTransform="translate3d("+moveX+"px)";
                 }else if(Math.abs(lenX)<iw/2){
-                    bigBox.style.webkitTransform="translateX("+moveX+"px)";
+                    bigBox.style.webkitTransform="translate3d("+moveX+"px)";
                 }
             }
             document.removeEventListener(mousemove,move);
